396a5dc776dc6c17ba9c4a444829fb3a631c6c00
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-09-28 12:11:01 +0530

    update work section css

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,25 +2,38 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import data from "../assets/data.json";
+import BuyNow from "../assets/BuyNow.png";
+import Monsoonmate from "../assets/Monsoonmate.png";
+import TaskTracker from "../assets/TaskTracker.png";
+import Portfolio_HTML from "../assets/Portfolio HTML CSS JS.png";
+import Portfolio_React from "../assets/Portfolio React.png";
 
 const Work = () => {
+  const img = [
+    BuyNow,
+    Monsoonmate,
+    TaskTracker,
+    Portfolio_HTML,
+    Portfolio_React,
+  ];
+
   return (
     <div id="work">
       <h2>Project</h2>
       <section>
         <article>
           <Carousel
-            showArrows={false}
-            showIndicators={false}
+            showArrows={true}
+            showIndicators={true}
             showStatus={false}
             showThumbs={false}
             interval={2000}
             infiniteLoop={true}
             autoPlay={true}
           >
-            {data.projects.map((i) => (
+            {data.projects.map((i, index) => (
               <div key={i.title} className="workItem">
-                <img src={i.imgSrc} alt={i.title} />
+                <img src={img[index]} alt={i.title} />
                 <aside>
                   <h3>{i.title}</h3>
                   <p>{i.description}</p>
